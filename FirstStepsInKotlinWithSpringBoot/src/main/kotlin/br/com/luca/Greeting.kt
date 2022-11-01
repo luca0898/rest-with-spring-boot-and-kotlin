def049c1cd60dc4ae96dd4c021df29418048af49
luca0898 <lucaguiraldello@hotmail.com>
2022-11-01 18:19:08 -0300

    Estrutura inicial e controlador de saudação

---
+++ FirstStepsInKotlinWithSpringBoot/src/main/kotlin/br/com/luca/Greeting.kt
@@ -0,0 +1,3 @@
+package br.com.luca
+
+class Greeting (val id: Long, val content: String)
---
+++ FirstStepsInKotlinWithSpringBoot/src/test/kotlin/br/com/luca/StartupTests.kt
@@ -0,0 +1,13 @@
+package br.com.luca
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
